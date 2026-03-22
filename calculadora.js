dc01310a6ed3176adfaad3371c53ab0440970ce4
Sandir <sandirr@gmail.com>
2026-03-22 20:01:05 -0300

    feat: adiciona função de subtração

--- calculadora.js
+++ calculadora.js
@@ -2,4 +2,10 @@ function somar(a, b) {
     // Retorna o resultado da adição matemática de 'a' mais 'b'
     return a + b;
 }
+function subtrair(a, b) {
+    // Retorna o resultado da subtração de 'a' menos 'b'
+    return a - b;
+}
 console.log("A soma de 5 e 3 é:", somar(5, 3));
+// Adicionamos um novo log para testar a função de subtração
+console.log("A subtração de 10 e 4 é:", subtrair(10, 4));
